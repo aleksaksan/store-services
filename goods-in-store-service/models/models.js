957593b88a2f9b49f9766225698fec445b310878
Aleksan <sanyok.10rus@gmail.com>
2024-08-30 21:12:42 +0300

    created db models

---
+++ goods-in-store-service/models/models.js
@@ -0,0 +1,26 @@
+const sequelize = require('../db');
+const { DataTypes } = require('sequelize');
+
+const Product = sequelize.define('product', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  plu: { type: DataTypes.STRING, allowNull: false, unique: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  }, { 
+    timestamps: false 
+  }
+);
+
+const Store = sequelize.define('store', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+},
+{ timestamps: false });
+
+const Stock = sequelize.define('stock', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  store_quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
+  order_quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
+}, { timestamps: false });
+
+Stock.HasMany(Product);
+Stock.HasOne(Store);
